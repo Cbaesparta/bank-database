dd0eebc0d9d5a161199549a0f903fcdeb66c8768
Sai Sankar Jena <saisankarjena4568@gmail.com>
2025-09-22 13:00:12 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,85 @@
+-- Banking Database Schema
+
+-- 1. BANK Table
+CREATE TABLE BANK(
+    bank_ID NUMERIC PRIMARY KEY,
+    bank_name VARCHAR(20) NOT NULL,
+    bank_address VARCHAR(20)
+);
+
+INSERT INTO BANK VALUES (001,'SBI','Godha colony');
+INSERT INTO BANK VALUES (002,'PNB','Rajwada');
+INSERT INTO BANK VALUES (003,'MET','Geeta colony');
+INSERT INTO BANK VALUES (004,'SWISS','Baker street');
+INSERT INTO BANK VALUES (005,'NXI','Putin Hall');
+
+SELECT * FROM BANK;
+
+-- 2. BRANCH Table
+CREATE TABLE BRANCH(
+    bank_ID NUMERIC,
+    branch_ID NUMERIC PRIMARY KEY,
+    branch_name VARCHAR(20) NOT NULL,
+    branch_address VARCHAR(20),
+    FOREIGN KEY(bank_ID) REFERENCES BANK(bank_ID)
+);
+
+INSERT INTO BRANCH VALUES (001,101,'Delhi','Gandhi Nagar');
+INSERT INTO BRANCH VALUES (002,102,'Punjab','Chandigarh');
+INSERT INTO BRANCH VALUES (003,103,'M.P','Vijay Nagar');
+INSERT INTO BRANCH VALUES (004,104,'Bangalore','Thullianadammm');
+INSERT INTO BRANCH VALUES (005,105,'Assam','Kaziranga');
+
+SELECT * FROM BRANCH;
+
+-- 3. ACCOUNT Table
+CREATE TABLE ACCOUNT(
+    account_ID NUMERIC PRIMARY KEY,
+    branch_ID NUMERIC,
+    name VARCHAR(20) NOT NULL,
+    account_type VARCHAR(20) NOT NULL,
+    FOREIGN KEY(branch_ID) REFERENCES BRANCH(branch_ID)
+);
+
+INSERT INTO ACCOUNT VALUES (201,101,'Rajeev','Salary account');
+INSERT INTO ACCOUNT VALUES (202,102,'Ravnish','Saving account');
+INSERT INTO ACCOUNT VALUES (203,103,'Naseeb','Fixed deposit account');
+INSERT INTO ACCOUNT VALUES (204,104,'Ashish','Recurring deposit account');
+INSERT INTO ACCOUNT VALUES (205,105,'Tanmay','NRI account');
+
+SELECT * FROM ACCOUNT;
+
+-- 4. LOAN Table
+CREATE TABLE LOAN(
+    account_ID NUMERIC,
+    loan_ID NUMERIC PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    loan_type VARCHAR(20) NOT NULL,
+    loan_amount NUMERIC,
+    FOREIGN KEY(account_ID) REFERENCES ACCOUNT(account_ID)
+);
+
+INSERT INTO LOAN VALUES (201,301,'Rajeev','Personal loan',10000);
+INSERT INTO LOAN VALUES (202,302,'Ravnish','Home loan',20000);
+INSERT INTO LOAN VALUES (203,303,'Naseeb','Car loan',15000);
+INSERT INTO LOAN VALUES (204,304,'Ashish','Education loan',25000);
+INSERT INTO LOAN VALUES (205,305,'Tanmay','Business loan',30000);
+
+SELECT * FROM LOAN;
+
+-- 5. CLIENT Table
+CREATE TABLE CLIENT(
+    account_ID NUMERIC,
+    client_ID NUMERIC PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    client_address VARCHAR(500) NOT NULL,
+    FOREIGN KEY(account_ID) REFERENCES ACCOUNT(account_ID)
+);
+
+INSERT INTO CLIENT VALUES (201,401,'Rajeev','21,Gandhi Nagar');
+INSERT INTO CLIENT VALUES (202,402,'Ravnish','16,Vijay Nagar');
+INSERT INTO CLIENT VALUES (203,403,'Sumit','13,Geeta colony');
+INSERT INTO CLIENT VALUES (204,404,'Naseeb','11, Savedh Nagar');
+INSERT INTO CLIENT VALUES (205,405,'Tanmay','07,godha colony');
+
+SELECT * FROM CLIENT;
